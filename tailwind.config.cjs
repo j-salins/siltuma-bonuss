1bb5e3866b89cbdc036550e05f4cfb6d31a59bf3
Janis Salins <janis.salins@gmail.com>
2022-12-16 18:22:00 +0200

    dist and config update

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,9 @@
 module.exports = {
   content: [
     "./index.html",
+    "./dist/index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./dist/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
